ee196d1d6ed543d7dcfcceb60108b91db70c7bf1
Jonathan Hooper <jonathan.hooper@gsa.gov>
2017-05-17 16:20:24 -0500

    Flatten API reponse objects
    This commit masks the structure of our data from end users by flattening
    the response into a single object, instead of an object with a nested
    data object.

--- src/app.js
+++ src/app.js
@@ -10,7 +10,13 @@ app.use(apiDataGovFilter)
 const fetchData = (req, res) => {
   const params = Object.assign(req.query, req.params)
   db.query(params).then(result => {
-    res.json(result)
+    response = result.map(dataPoint => Object.assign({
+      id: dataPoint.id,
+      date_time: dataPoint.date_time,
+      report_name: dataPoint.report_name,
+      report_agency: dataPoint.report_agency,
+    }, dataPoint.data))
+    res.json(response)
   }).catch(err => {
     logger.error("Unexpected Error:", err)
     res.status(400)
