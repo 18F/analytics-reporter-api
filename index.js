09cace64a9447cdbbded2e26cdd2e6d857199da4
Ryan Wold <ryan.wold@gsa.gov>
2021-04-19 15:39:41 -0700

    add New Relic

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const app = require('./src/app');
 const config = require('./src/config');
 const logger = require('./src/logger');
 
+if (process.env.NEW_RELIC_APP_NAME) {
+	console.log("Starting New Relic");
+	require("newrelic");
+}
+
 app.listen(config.port, () => {
   console.log(`Listening on ${config.port}`);
 });
