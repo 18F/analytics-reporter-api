b0e1e262f7572db7963a062b0d903a79e8ae174c
Ryan Wold <ryan.wold@gsa.gov>
2023-03-24 15:20:55 -0700

    for consistency

--- test/support/db.js
+++ test/support/db.js
@@ -1,10 +1,10 @@
 const knex = require('knex');
 const config = require('../../src/config');
 
-const client = knex({ client: 'pg', connection: config.postgres });
+const db = knex({ client: 'pg', connection: config.postgres });
 
 const resetSchema = () => {
-  return client.table("analytics_data").delete();
+  return db('analytics_data').delete();
 };
 
-module.exports = { client, config, resetSchema };
+module.exports = { db, config, resetSchema };
