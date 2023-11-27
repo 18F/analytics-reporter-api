a00b4606d89ca49826cd0ed77da89b134f50d7c6
Xavier Metichecchia <XavierMetichecchia>
2023-11-27 11:37:17 -0500

    Rework environmental variables for database

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,6 @@
-#!/bin/sh
-#Set Postgres env vars from VCAP_SERVICES
-export POSTGRES_DATABASE=$(echo $VCAP_SERVICES | jq -r '."aws-rds"[].credentials.db_name')
-export POSTGRES_HOST=$(echo $VCAP_SERVICES | jq -r '."aws-rds"[].credentials.host')
-export POSTGRES_PASSWORD=$(echo $VCAP_SERVICES | jq -r '."aws-rds"[].credentials.password')
-export POSTGRES_USER=$(echo $VCAP_SERVICES | jq -r '."aws-rds"[].credentials.username')
-
-
-#This line will get sed replaced based on the environment/branch
+#!/bin/bash
 export NEW_RELIC_APP_NAME="analytics-reporter-api"
 export NODE_ENV="test"
+export PATH="$PATH:/home/vcap/deps/0/bin"
+node deploy/cron.js
+
