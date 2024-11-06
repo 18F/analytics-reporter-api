0436acbbcf7f19727dff0b789e51bb9d7040f034
Michael Levin <michael.levin@gsa.gov>
2024-11-06 10:29:35 -0500

    [Tech Debt] Update supertest, use async/await in tests

--- test/app.test.js
+++ test/app.test.js
@@ -31,7 +31,7 @@ routes.forEach((route) => {
       db.query = () => Promise.resolve();
     });
 
-    it("should pass params from the url to db.query and render the result", (done) => {
+    it("should pass params from the url to db.query and render the result", async () => {
       db.query = (params) => {
         expect(params.reportAgency).to.equal("fake-agency");
         expect(params.reportName).to.equal("fake-report");
@@ -46,19 +46,16 @@ routes.forEach((route) => {
         .get(`/${route}/agencies/fake-agency/reports/fake-report/data`)
         .expect(200);
 
-      dataRequest
-        .then((actualResponse) => {
-          const expectedResponseBody = handleIfRouteNotice(route, [
-            { id: 1, date: "2017-01-01" },
-            { id: 2, date: "2017-01-02" },
-          ]);
-          expect(actualResponse.body).to.deep.equal(expectedResponseBody);
-          done();
-        })
-        .catch(done);
+      await dataRequest.then((actualResponse) => {
+        const expectedResponseBody = handleIfRouteNotice(route, [
+          { id: 1, date: "2017-01-01" },
+          { id: 2, date: "2017-01-02" },
+        ]);
+        expect(actualResponse.body).to.deep.equal(expectedResponseBody);
+      });
     });
 
-    it("should not pass the agency param if the request does not specify an agency", (done) => {
+    it("should not pass the agency param if the request does not specify an agency", async () => {
       db.query = (params) => {
         expect(params.reportAgency).to.be.undefined;
         expect(params.reportName).to.equal("fake-report");
@@ -73,19 +70,16 @@ routes.forEach((route) => {
         .get(`/${route}/reports/fake-report/data`)
         .expect(200);
 
-      dataRequest
-        .then((actualResponse) => {
-          const expectedResponseBody = handleIfRouteNotice(route, [
-            { id: 1, date: "2017-01-01" },
-            { id: 2, date: "2017-01-02" },
-          ]);
-          expect(actualResponse.body).to.deep.equal(expectedResponseBody);
-          done();
-        })
-        .catch(done);
+      await dataRequest.then((actualResponse) => {
+        const expectedResponseBody = handleIfRouteNotice(route, [
+          { id: 1, date: "2017-01-01" },
+          { id: 2, date: "2017-01-02" },
+        ]);
+        expect(actualResponse.body).to.deep.equal(expectedResponseBody);
+      });
     });
 
-    it("should merge the params in the url with query params", (done) => {
+    it("should merge the params in the url with query params", async () => {
       db.query = (params) => {
         expect(params.reportAgency).to.equal("fake-agency");
         expect(params.reportName).to.equal("fake-report");
@@ -101,19 +95,16 @@ routes.forEach((route) => {
         .get(`/${route}/agencies/fake-agency/reports/fake-report/data?limit=50`)
         .expect(200);
 
-      dataRequest
-        .then((actualResponse) => {
-          const expectedResponseBody = handleIfRouteNotice(route, [
-            { id: 1, date: "2017-01-01" },
-            { id: 2, date: "2017-01-02" },
-          ]);
-          expect(actualResponse.body).to.deep.equal(expectedResponseBody);
-          done();
-        })
-        .catch(done);
+      await dataRequest.then((actualResponse) => {
+        const expectedResponseBody = handleIfRouteNotice(route, [
+          { id: 1, date: "2017-01-01" },
+          { id: 2, date: "2017-01-02" },
+        ]);
+        expect(actualResponse.body).to.deep.equal(expectedResponseBody);
+      });
     });
 
-    it("should respond with a 400 if db.query rejects", (done) => {
+    it("should respond with a 400 if db.query rejects", async () => {
       db.query = () =>
         Promise.reject("This is a test of the emergency broadcast system.");
 
@@ -121,19 +112,16 @@ routes.forEach((route) => {
         .get(`/${route}/agencies/fake-agency/reports/fake-report/data`)
         .expect(400);
 
-      dataRequest
-        .then((actualResponse) => {
-          const expectedResponseBody = {
-            message: "An error occurred. Please check the application logs.",
-            status: 400,
-          };
-          expect(actualResponse.body).to.deep.equal(expectedResponseBody);
-          done();
-        })
-        .catch(done);
+      await dataRequest.then((actualResponse) => {
+        const expectedResponseBody = {
+          message: "An error occurred. Please check the application logs.",
+          status: 400,
+        };
+        expect(actualResponse.body).to.deep.equal(expectedResponseBody);
+      });
     });
 
-    it("should respond with a 400 if the domain report is not one of the acceptable kinds of reports", (done) => {
+    it("should respond with a 400 if the domain report is not one of the acceptable kinds of reports", async () => {
       db.query = (params) => {
         expect(params.domain).to.equal("fakeiscool.gov");
         expect(params.reportName).to.equal("browser");
@@ -155,20 +143,17 @@ routes.forEach((route) => {
         .get(`/${route}/domain/fakeiscool.gov/reports/browser/data`)
         .expect(400);
 
-      dataRequest
-        .then((actualResponse) => {
-          const expectedResponseBody = {
-            message:
-              "You are requesting a report that cannot be filtered on domain. Please try one of the following reports: site, domain, download, second-level-domain.",
-            status: 400,
-          };
-          expect(actualResponse.body).to.deep.equal(expectedResponseBody);
-          done();
-        })
-        .catch(done);
+      await dataRequest.then((actualResponse) => {
+        const expectedResponseBody = {
+          message:
+            "You are requesting a report that cannot be filtered on domain. Please try one of the following reports: site, domain, download, second-level-domain.",
+          status: 400,
+        };
+        expect(actualResponse.body).to.deep.equal(expectedResponseBody);
+      });
     });
 
-    it("should pass params from the url to db.query and render the result for a domain query for a non-download report", (done) => {
+    it("should pass params from the url to db.query and render the result for a domain query for a non-download report", async () => {
       db.query = (params) => {
         expect(params.domain).to.equal("fakeiscool.gov");
         expect(params.reportName).to.equal("site");
@@ -187,20 +172,17 @@ routes.forEach((route) => {
         .get(`/${route}/domain/fakeiscool.gov/reports/site/data`)
         .expect(200);
 
-      dataRequest
-        .then((actualResponse) => {
-          const expectedResponseBody = handleIfRouteNotice(route, [
-            {
-              id: 1,
-              date: "2017-01-01",
-              report_name: "site",
-              domain: "fakeiscool.gov",
-            },
-          ]);
-          expect(actualResponse.body).to.deep.equal(expectedResponseBody);
-          done();
-        })
-        .catch(done);
+      await dataRequest.then((actualResponse) => {
+        const expectedResponseBody = handleIfRouteNotice(route, [
+          {
+            id: 1,
+            date: "2017-01-01",
+            report_name: "site",
+            domain: "fakeiscool.gov",
+          },
+        ]);
+        expect(actualResponse.body).to.deep.equal(expectedResponseBody);
+      });
     });
   });
 });
@@ -210,7 +192,7 @@ describe(`app with unspupported version`, () => {
     db.query = () => Promise.resolve();
   });
 
-  it("should not accept unsupported versions", (done) => {
+  it("should not accept unsupported versions", async () => {
     const unsupportedVersion = "v2.x";
 
     db.query = (params) => {
@@ -232,15 +214,12 @@ describe(`app with unspupported version`, () => {
       )
       .expect(404);
 
-    dataRequest
-      .then((actualResponse) => {
-        const expectedResponse = {
-          _body: expectedErrorMessage,
-          status: 404,
-        };
-        expect(actualResponse).to.include(expectedResponse);
-        done();
-      })
-      .catch(done);
+    await dataRequest.then((actualResponse) => {
+      const expectedResponse = {
+        _body: expectedErrorMessage,
+        status: 404,
+      };
+      expect(actualResponse).to.include(expectedResponse);
+    });
   });
 });
